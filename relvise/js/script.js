92158ea188d078b0fc65a6cb29b5558c8b6201a6
KombinatorVit <slonik0775@gmail.com>
2023-06-08 22:27:50 +0300

    update + news project

--- relvise/js/script.js
+++ relvise/js/script.js
@@ -1,4 +1,18 @@
 "use strict"
 
 
-console.log(new Date())
+let arr = [7, 3, 5, 134,  8 , 1]
+
+arr.sort((a, b) =>{
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0
+})
+
+
+
+
+let obj = {x: 1, y: 2, z: 3};
+
+
+console.log(Object.keys(obj));
