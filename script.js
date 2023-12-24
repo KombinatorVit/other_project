59993e0aa6c47e1064ef6e94d64dc47f8518e854
KombinatorVit <slonik0775@gmail.com>
2023-12-24 22:57:25 +0200

    update project

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict"
+
+
+const INTERVAL = 500
+let counter = 0
+const MAX_VALUE = 10
+let timer = null
+
+const event = () => {
+
+    if (counter === MAX_VALUE) {
+        console.log('THE END')
+        clearTimeout(timer)
+        return
+    }
+    console.dir({counter, date: Date.now()})
+    counter++
+}
+
+console.log('Begin')
+timer = setInterval(event, INTERVAL)
