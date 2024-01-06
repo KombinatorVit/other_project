7b164668387298db57be0cb3080a59d4f55d4327
KombinatorVit <slonik0775@gmail.com>
2024-01-06 16:45:11 +0200

    add new marketing section

--- party/marketing/js/index.js
+++ party/marketing/js/index.js
@@ -4,4 +4,15 @@ $(function () {
         arrows: false,
 
     });
+
+
+    var mixer = mixitup('.portfolio__content');
+
+
+    $('.portfolio__filter-btn-item').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active')
+
+    })
+
+
 })
