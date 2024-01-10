44af2e363316b51ae33d420c9b54e2a3f87cdb8f
KombinatorVit <slonik0775@gmail.com>
2024-01-10 21:03:44 +0200

    Candler add new sections and new project Panto

--- candler/js/main.js
+++ candler/js/main.js
@@ -9,3 +9,43 @@ menuBtn.addEventListener('click', () => {
 
 })
 
+const swiperOne = new Swiper('.feedback__slider', {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    mousewheel: true,
+
+
+});
+
+const swiperTwo = new Swiper('.certificate__slider', {
+    loop: true,
+    spaceBetween: 20,
+    mousewheel: true,
+    slidesPerView: 3,
+    breakpoints: {
+    
+        501: {
+            slidesPerView: 3,
+        },
+        480: {
+            slidesPerView: 2,
+
+        },
+        400: {
+            slidesPerView: 1,
+        },
+
+    },
+
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+
+});
